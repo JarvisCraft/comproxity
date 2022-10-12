a18abc6f45a00991fab8b1b8b139526507bf0604
Petr Portnov <gh@progrm-jarvis.ru>
2022-10-12 23:52:29 +0300

    feat(GH-1): implement simple reverse HTTP proxy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+mod proxy;
+
+use config::Config;
+use hyper::server::conn::AddrStream;
+use serde::Deserialize;
+use std::error::Error;
+use std::{convert::Infallible, net::SocketAddr};
+
+/// Configuration of Comproxity.
+#[derive(Deserialize)]
+struct ComproxityConfig {
+    /// Address to which the proxy should bind such as `127.0.0.1:8000`.
+    #[serde(default = "default_address")]
+    address: SocketAddr,
+
+    /// Endpoint calls to which should be proxied such as `http://127.0.0.1:4321`.
+    endpoint: String,
+}
+
+fn default_address() -> SocketAddr {
+    ([127, 0, 0, 1], 8000).into()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
+    let config: ComproxityConfig = Config::builder()
+        .add_source(config::File::with_name("config"))
+        .add_source(config::Environment::with_prefix("COMPROXITY"))
+        .build()?
+        .try_deserialize()?;
+
+    // TODO: find a more elegant solution to share endpoint
+    let endpoint: &'static str = Box::leak(config.endpoint.clone().into_boxed_str());
+
+    let server = hyper::Server::bind(&config.address)
+        .serve(hyper::service::make_service_fn(
+            move |connection: &AddrStream| {
+                let client_address = connection.remote_addr();
+                async move {
+                    Ok::<_, Infallible>(hyper::service::service_fn(move |request| {
+                        proxy::handle_request(endpoint, client_address, request)
+                    }))
+                }
+            },
+        ))
+        .with_graceful_shutdown(async {
+            if let Err(error) = tokio::signal::ctrl_c().await {
+                tracing::error!("Failed to handle CTRL-C signal: {error}");
+            }
+            tracing::info!("Shutting down");
+        });
+
+    tracing::info!("Starting");
+
+    server.await?;
+
+    Ok(())
 }
---
+++ src/proxy.rs
@@ -0,0 +1,41 @@
+//! Implementation of the reverse proxy.
+
+use {
+    hyper::{Body, Request, Response, StatusCode},
+    std::{convert::Infallible, net::SocketAddr},
+    ulid::Ulid,
+};
+
+pub(crate) async fn handle_request(
+    forward_uri: &str,
+    client_address: SocketAddr,
+    request: Request<Body>,
+) -> Result<Response<Body>, Infallible> {
+    handle_unique_request(forward_uri, client_address, request, Ulid::new()).await
+}
+
+#[tracing::instrument(target = "proxy", name = "request", skip(forward_uri, request))]
+async fn handle_unique_request(
+    forward_uri: &str,
+    client_address: SocketAddr,
+    request: Request<Body>,
+    id: Ulid,
+) -> Result<Response<Body>, Infallible> {
+    tracing::info!("Handling {request:?}");
+
+    match hyper_reverse_proxy::call(client_address.ip(), forward_uri, request).await {
+        Ok(response) => {
+            tracing::debug!("Responding with {response:?}");
+            Ok(response)
+        }
+        Err(error) => {
+            tracing::error!("Failed to handle request: {error:?}");
+            Ok(Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from(format!(
+                    "Request {id} failed due to an internal error"
+                )))
+                .unwrap())
+        }
+    }
+}
