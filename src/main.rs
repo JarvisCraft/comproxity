ac431a6e3bf962081a7170b2f4602aeee70eadcb
Yaroslav Bolyukin <iam@lach.pw>
2022-10-15 21:38:56 +0200

    fix: do not leak endpoint address
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,51 @@
+use hyper::server::conn::AddrStream;
+use std::{convert::Infallible, process::ExitCode};
+use tracing::{error, info};
+
+mod config;
 mod proxy;
 
 use config::Config;
-use hyper::server::conn::AddrStream;
-use serde::Deserialize;
-use std::{convert::Infallible, net::SocketAddr};
-
-/// Configuration of Comproxity.
-#[derive(Deserialize)]
-struct ComproxityConfig {
-    /// Address to which the proxy should bind such as `127.0.0.1:8000`.
-    #[serde(default = "default_address")]
-    address: SocketAddr,
-
-    /// Endpoint calls to which should be proxied such as `http://127.0.0.1:4321`.
-    endpoint: String,
-}
-
-fn default_address() -> SocketAddr {
-    ([127, 0, 0, 1], 8000).into()
-}
 
 #[tokio::main]
 #[tracing::instrument(name = "bootstrap")]
-async fn main() {
+async fn main() -> ExitCode {
     tracing_subscriber::fmt::init();
 
-    let config: ComproxityConfig = match Config::builder()
-        .add_source(config::File::with_name("config"))
-        .add_source(config::Environment::with_prefix("COMPROXITY"))
-        .build()
-        .and_then(Config::try_deserialize)
-    {
+    let config = match Config::load() {
         Ok(config) => config,
-        Err(error) => {
-            tracing::error!("Failed to parse config: {error}");
-            return;
+        Err(e) => {
+            error!("failed to load config: {e}");
+            return ExitCode::FAILURE;
         }
     };
-
-    // TODO: find a more elegant solution to share endpoint
-    let endpoint: &'static str = Box::leak(config.endpoint.clone().into_boxed_str());
 
     let server = hyper::Server::bind(&config.address)
         .serve(hyper::service::make_service_fn(
             move |connection: &AddrStream| {
                 let client_address = connection.remote_addr();
+                let config = config.clone();
                 async move {
                     Ok::<_, Infallible>(hyper::service::service_fn(move |request| {
-                        proxy::handle_request(endpoint, client_address, request)
+                        let config = config.clone();
+                        proxy::handle_request(config, client_address, request)
                     }))
                 }
             },
         ))
         .with_graceful_shutdown(async {
             if let Err(error) = tokio::signal::ctrl_c().await {
-                tracing::error!("Failed to handle CTRL-C signal: {error}");
+                error!("Failed to handle CTRL-C signal: {error}");
             }
-            tracing::info!("Shutting down");
+            info!("Shutting down");
         });
 
-    tracing::info!("Starting");
+    info!("Starting");
 
     if let Err(error) = server.await {
-        tracing::error!("Server failed: {error}");
-    }
+        error!("Server failed: {error}");
+        return ExitCode::FAILURE;
+    };
+
+    ExitCode::SUCCESS
 }
